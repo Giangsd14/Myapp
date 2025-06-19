3d1f404ac09da5e9b7e493a5b7482d0fbe8eb6d8
TGiang <giang1462004@gmail.com>
2025-06-19 16:50:21 +0700

    #16 - add CROS for Fontend

--- MyApp/myapp/main.py
+++ MyApp/myapp/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from .routers import login, user, map
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(docs_url="/")
 
+# origins = [
+#     "https://my-app.netlify.app",   # Domain Frontend
+#     "http://localhost:8100",        # Ionic local
+# ]
+
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=origins,  # or ["*"] for test
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(login.router)
 app.include_router(map.router)
